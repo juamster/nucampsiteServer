345224a01119521543d22c5a6e281b481e119b51
Judy Amster <judyamster@icloud.com>
2021-12-30 11:02:10 -0500

    populate

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const User = require("./models/users");
+const User = require("./models/user");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/users");
+const User = require("../models/user");
 const passport = require("passport");
 const authenticate = require("../authenticate");
 
@@ -14,16 +14,30 @@ router.post("/signup", (req, res) => {
   User.register(
     new User({ username: req.body.username }),
     req.body.password,
-    (err) => {
+    (err, user) => {
       if (err) {
         res.statusCode = 500;
         res.setHeader("Content-Type", "application/json");
         res.json({ err: err });
       } else {
-        passport.authenticate("local")(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json({ success: true, status: "Registration Successful!" });
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save((err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration Successful!" });
+          });
         });
       }
     }
